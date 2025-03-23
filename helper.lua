a93ad8daaaf25a00e00d568c2034f78b489465fc
xavixava <xavier.antunes@tecnico.ulisboa.pt>
2025-03-23 15:34:47 +0000

    Updated helper.lua, so it only prints whether or not the anysec dissector will work

--- helper.lua
+++ helper.lua
@@ -1,4 +1,11 @@
-local dissector_list = Dissector.list()
-for i = 1,#dissector_list do
-    print(dissector_list[i])
+-- local dissector_list = Dissector.list()
+local macsec_dissector = Dissector.get("macsec")
+if macsec_dissector then
+    print("MACsec dissector is callable")
+else
+    print("MACsec dissector is not callable. This dissector is not usable")
 end
+
+-- for i = 1,#dissector_list do
+--     print(dissector_list[i])
+-- end
